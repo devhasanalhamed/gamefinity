24a865ac7640bd1b768a9fc2ea348a755c63b0aa
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-08-28 07:04:05 +0300

    Create sale_widget.dart
    new widget to scroll sales

---
+++ lib/widgets/sale_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class SaleWidget extends StatefulWidget {
+  const SaleWidget({Key? key}) : super(key: key);
+
+  @override
+  SaleWidgetState createState() => SaleWidgetState();
+}
+
+class SaleWidgetState extends State<SaleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18.0),
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFF7A60A5),
+            Color(0xFF82C3DF),
+          ],
+          begin: FractionalOffset(0.0, 0.0),
+          end: FractionalOffset(1.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9689CE),
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Get the special discount',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18.0,
+                      ),
+                      Flexible(
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text(
+                              '50% OFF',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 100.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.network(
+                'https://www.pngfind.com/pngs/m/35-356412_running-shoes-png-image-sports-shoes-images-png.png',
+                width: double.infinity,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
