4d0bb442961717c5a5e41cda9908a869338614cd
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-08-30 06:16:37 +0300

    Create product_widget.dart

---
+++ lib/widgets/product_widget.dart
@@ -0,0 +1,96 @@
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:gamefinity/screens/product_details_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+class ProductWidget extends StatefulWidget {
+  const ProductWidget({Key? key}) : super(key: key);
+
+  @override
+  ProductWidgetState createState() => ProductWidgetState();
+}
+
+class ProductWidgetState extends State<ProductWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      borderRadius: BorderRadius.circular(8.0),
+      color: Theme.of(context).cardColor,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(8.0),
+        onTap: () {
+          Navigator.push(
+            context,
+            PageTransition(
+              type: PageTransitionType.fade,
+              child: const ProductDetailsScreen(),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 5,
+                right: 5,
+                top: 8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: RichText(
+                      text: const TextSpan(
+                        text: '\$',
+                        style: TextStyle(
+                          color: Color.fromRGBO(33, 150, 243, 1),
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '168.00',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Icon(Icons.favorite),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: FancyShimmerImage(
+                imageUrl:
+                    'https://images.dsw.com/is/image/DSWShoes/533780_035_ss_01?impolicy=qlt-medium-high&imwidth=640&imdensity=2',
+                height: size.height * 0.2,
+                width: double.infinity,
+                boxFit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Title',
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            SizedBox(height: size.height * 0.01)
+          ],
+        ),
+      ),
+    );
+  }
+}
