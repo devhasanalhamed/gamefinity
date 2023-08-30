caad829661f313293742379c5a5d9f1690ec4722
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-08-30 06:15:37 +0300

    Update homepage_screen.dart

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,5 +1,10 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:gamefinity/screens/all_products_screen.dart';
+import 'package:gamefinity/screens/category_screen.dart';
+import 'package:gamefinity/widgets/product_widget.dart';
 import 'package:gamefinity/widgets/sale_widget.dart';
+import 'package:page_transition/page_transition.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -27,10 +32,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                    child: const CategoryScreen(),
+                    type: PageTransitionType.fade,
+                  ));
+            },
+            icon: const Icon(
+              Icons.category,
+            ),
+          ),
           actions: [
             IconButton.outlined(
               onPressed: () {},
@@ -48,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SizedBox(height: 18.0),
+              const SizedBox(height: 18.0),
               TextField(
                 controller: textEditingController,
                 keyboardType: TextInputType.text,
@@ -70,7 +90,62 @@ class _MyHomePageState extends State<MyHomePage> {
                   suffixIcon: const Icon(Icons.search),
                 ),
               ),
-              const SaleWidget(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) => const SaleWidget(),
+                          pagination: const SwiperPagination(
+                            builder: DotSwiperPaginationBuilder(
+                              activeColor: Colors.red,
+                              color: Colors.white,
+                            ),
+                          ),
+                          autoplay: true,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('All Games 🔥'),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                PageTransition(
+                                  child: const AllProductsScreen(),
+                                  type: PageTransitionType.fade,
+                                ),
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.arrow_circle_right_outlined,
+                            ),
+                          ),
+                        ],
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 3,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.6,
+                          crossAxisSpacing: 0.0,
+                          mainAxisSpacing: 0.0,
+                        ),
+                        itemBuilder: (context, index) {
+                          return const ProductWidget();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
