b2d23020be89aab3462d7b011b237ca861b23682
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-02 06:34:01 +0300

    Update category_widget.dart
    enhancing category widget

--- lib/widgets/category_widget.dart
+++ lib/widgets/category_widget.dart
@@ -1,18 +1,39 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
+import 'package:gamefinity/global/theme.dart';
 
 class CategoryWidget extends StatelessWidget {
-const CategoryWidget({ Key? key }) : super(key: key);
+  const CategoryWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Stack(
-      children: [
-        FancyShimmerImage(
-          
-          imageUrl: '',
-        )
-      ],
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: FancyShimmerImage(
+              height: size.width * 0.45,
+              width: size.width * 0.45,
+              imageUrl:
+                  'https://picsum.photos/200',
+              boxFit: BoxFit.fill,
+            ),
+          ),
+          Text(
+            'categ name',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              backgroundColor: lightCardColor.withOpacity(0.5),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
