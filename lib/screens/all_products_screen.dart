44987770531777c389fd5f20fb16129f39fc5b77
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-03 07:26:00 +0300

    Update all_products_screen.dart

--- lib/screens/all_products_screen.dart
+++ lib/screens/all_products_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:gamefinity/models/products_model.dart';
 import 'package:gamefinity/widgets/product_widget.dart';
 
 class AllProductsScreen extends StatelessWidget {
-  const AllProductsScreen({Key? key}) : super(key: key);
+  final List<ProductsModel> productsList;
+  const AllProductsScreen({
+    Key? key,
+    required this.productsList,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +15,28 @@ class AllProductsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('All products'),
       ),
-      body: GridView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 3,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.6,
-          crossAxisSpacing: 0.0,
-          mainAxisSpacing: 0.0,
-        ),
-        itemBuilder: (context, index) {
-          return const ProductWidget();
-        },
-      ),
+      body: productsList.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+              ),
+            )
+          : GridView.builder(
+              shrinkWrap: true,
+              itemCount: productsList.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.6,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+              ),
+              itemBuilder: (context, index) {
+                return ProductWidget(
+                  title: productsList[index].title!,
+                  imgUrl: productsList[index].images![0],
+                );
+              },
+            ),
     );
   }
 }
