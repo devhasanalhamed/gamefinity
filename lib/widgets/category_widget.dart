92ed5dfc21ba7694e5beb43d4590f8786f2de4e2
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-04 06:41:35 +0300

    Update category_widget.dart
    replacing dummy data with data from the server

--- lib/widgets/category_widget.dart
+++ lib/widgets/category_widget.dart
@@ -1,12 +1,15 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gamefinity/global/theme.dart';
+import 'package:gamefinity/models/category_model.dart';
+import 'package:provider/provider.dart';
 
 class CategoryWidget extends StatelessWidget {
   const CategoryWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    CategoriesModel category = Provider.of<CategoriesModel>(context);
     Size size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -18,13 +21,12 @@ class CategoryWidget extends StatelessWidget {
             child: FancyShimmerImage(
               height: size.width * 0.45,
               width: size.width * 0.45,
-              imageUrl:
-                  'https://picsum.photos/200',
+              imageUrl: category.image!,
               boxFit: BoxFit.fill,
             ),
           ),
           Text(
-            'categ name',
+            category.name!,
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 24.0,
