d76f5dce72d37ec12fd441532d80c57f8bf4dd89
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-04 06:40:27 +0300

    Update category_screen.dart
    link the categories from the server

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gamefinity/models/category_model.dart';
+import 'package:gamefinity/services/api_handler.dart';
 import 'package:gamefinity/widgets/category_widget.dart';
+import 'package:provider/provider.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
@@ -15,17 +18,39 @@ class _CategoryScreenState extends State<CategoryScreen> {
       appBar: AppBar(
         title: const Text('Categoires'),
       ),
-      body: GridView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 0.0,
-          mainAxisSpacing: 0.0,
-        ),
-        itemCount: 5,
-        itemBuilder: (context, index) => const CategoryWidget(),
-      ),
+      body: FutureBuilder<List<CategoriesModel>>(
+          future: APIHandler.getCategories(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.amber,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('An Error Occure'),
+              );
+            } else if (snapshot.data == null) {
+              return const Center(
+                child: Text('There are no proudcts yet'),
+              );
+            } else {
+              return GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 0.0,
+                  mainAxisSpacing: 0.0,
+                ),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => ChangeNotifierProvider.value(
+                    value: snapshot.data![index],
+                    child: const CategoryWidget()),
+              );
+            }
+          }),
     );
   }
 }
