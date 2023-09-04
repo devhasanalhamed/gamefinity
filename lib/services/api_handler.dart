59db5eebe023e6d8e309fc351d06e8991b8795b1
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-04 06:41:17 +0300

    Update api_handler.dart
    fetching categories from the server

--- lib/services/api_handler.dart
+++ lib/services/api_handler.dart
@@ -1,4 +1,5 @@
 import 'package:gamefinity/consts/api_consts.dart';
+import 'package:gamefinity/models/category_model.dart';
 import 'package:gamefinity/models/products_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -13,5 +14,16 @@ class APIHandler {
       temp.add(value);
     }
     return ProductsModel.productsFromSnapshot(temp);
+  }
+
+  static Future<List<CategoriesModel>> getCategories() async {
+    final uri = Uri.https(BASE_URL, 'api/v1/categories');
+    final response = await http.get(uri);
+    final responseData = jsonDecode(response.body);
+    List temp = [];
+    for (var value in responseData) {
+      temp.add(value);
+    }
+    return CategoriesModel.categoriesFromSnapshot(temp);
   }
 }
