85b57c03aa41a17cbda91c1c734c3b145d0a8d0a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-05 06:42:51 +0300

    Update developer_widget.dart
    replace the dummy data with the one from the server

--- lib/widgets/developer_widget.dart
+++ lib/widgets/developer_widget.dart
@@ -1,28 +1,31 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gamefinity/global/theme.dart';
+import 'package:gamefinity/models/users_model.dart';
+import 'package:provider/provider.dart';
 
 class DeveloperWidget extends StatelessWidget {
   const DeveloperWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UsersModel>(context);
     Size size = MediaQuery.of(context).size;
     return ListTile(
       leading: FancyShimmerImage(
         height: size.width * 0.15,
         width: size.width * 0.15,
-        imageUrl: '',
+        imageUrl: user.avatar!,
         errorWidget: const Icon(
           Icons.dangerous,
           size: 28.0,
         ),
         boxFit: BoxFit.fill,
       ),
-      title: Text('user name'),
-      subtitle: Text('example@email.com '),
+      title: Text(user.name!),
+      subtitle: Text(user.email!),
       trailing: Text(
-        'user role',
+        user.role!,
         style: TextStyle(
           color: lightIconsColor,
           fontWeight: FontWeight.bold,
