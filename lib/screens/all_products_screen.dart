2a6baf49b491e9a2a2bcff6b2651ae856bf03e6c
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-06 06:59:49 +0300

    Update all_products_screen.dart
    add pagination logic to the all products screen

--- lib/screens/all_products_screen.dart
+++ lib/screens/all_products_screen.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:gamefinity/models/products_model.dart';
+import 'package:gamefinity/services/api_handler.dart';
 import 'package:gamefinity/widgets/product_widget.dart';
 import 'package:provider/provider.dart';
 
-class AllProductsScreen extends StatelessWidget {
-  final List<ProductsModel> productsList;
-  const AllProductsScreen({
-    Key? key,
-    required this.productsList,
-  }) : super(key: key);
+class AllProductsScreen extends StatefulWidget {
+  const AllProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AllProductsScreen> createState() => _AllProductsScreenState();
+}
+
+class _AllProductsScreenState extends State<AllProductsScreen> {
+  List<ProductsModel> productsList = [];
+  int limit = 10;
+  bool _isLoading = false;
+  bool _isLimit = false;
+
+  final ScrollController _scrollcontroller = ScrollController();
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _scrollcontroller.addListener(
+      () async {
+        if (_scrollcontroller.position.pixels ==
+            _scrollcontroller.position.maxScrollExtent) {
+          _isLoading = true;
+          limit += 10;
+          await getData();
+          _isLoading = false;
+        }
+      },
+    );
+  }
+
+  Future<void> getData() async {
+    productsList = await APIHandler.getAllProducts(limit: '$limit');
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _scrollcontroller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,21 +64,33 @@ class AllProductsScreen extends StatelessWidget {
                 color: Colors.amber,
               ),
             )
-          : GridView.builder(
-              shrinkWrap: true,
-              itemCount: productsList.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.6,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  GridView.builder(
+                    controller: _scrollcontroller,
+                    itemCount: productsList.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.6,
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
+                    ),
+                    itemBuilder: (context, index) {
+                      return ChangeNotifierProvider.value(
+                        value: productsList[index],
+                        child: const ProductWidget(),
+                      );
+                    },
+                  ),
+                  const CircularProgressIndicator(
+                    color: Colors.amber,
+                  ),
+                ],
               ),
-              itemBuilder: (context, index) {
-                return ChangeNotifierProvider.value(
-                  value: productsList[index],
-                  child: const ProductWidget(),
-                );
-              },
             ),
     );
   }
