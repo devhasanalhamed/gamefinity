096d346983dadbd1b8a3c307c77f0b27f397069b
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-18 10:45:15 +0300

    Update firebase_api.dart

--- lib/core/helpers/firebase_api.dart
+++ lib/core/helpers/firebase_api.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:ui';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
