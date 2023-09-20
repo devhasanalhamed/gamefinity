8e728ef7702e2ec88f1384c876ce242137544478
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-20 17:33:02 +0300

    Update profile.dart

--- lib/mvc/views/widgets/profile.dart
+++ lib/mvc/views/widgets/profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gamefinity/core/helpers/size_config.dart';
+import 'package:gamefinity/generated/l10n.dart';
+import 'package:gamefinity/mvc/views/widgets/profile_pageview.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -10,14 +13,42 @@ class Profile extends StatefulWidget {
 class ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        ListTile(
-          leading: CircleAvatar(),
-          title: Text('username'),
-          subtitle: Text('subtitle'),
-        ),
-      ],
+    SizeConfig().init(context);
+    return Padding(
+      padding: EdgeInsets.only(
+        top: SizeConfig.safeBlockVertical! * 2,
+      ),
+      child: Stack(
+        children: [
+          // CustomPaint(
+          //   painter: CustomPainter(
+          //     repaint:
+          //   ),
+          // ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CircleAvatar(
+                radius: SizeConfig.safeBlockHorizontal! * 12,
+              ),
+              SizedBox(height: SizeConfig.safeBlockVertical! * 1),
+              const Text(
+                '@devhassanmohammed',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Expanded(
+                child: ProfilePageView(),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
