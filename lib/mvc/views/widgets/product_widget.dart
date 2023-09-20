b803ad70b51c079e8b94072410358bd3d9a5b20f
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-20 17:32:45 +0300

    Update product_widget.dart

--- lib/mvc/views/widgets/product_widget.dart
+++ lib/mvc/views/widgets/product_widget.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gamefinity/mvc/models/products_model.dart';
 import 'package:gamefinity/mvc/views/screens/product_details_screen.dart';
