aca497129fa787513370f976956f28cf4af022ae
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-25 13:39:04 +0300

    Update auth_provider.dart

--- lib/mvc/controllers/auth_provider.dart
+++ lib/mvc/controllers/auth_provider.dart
@@ -6,48 +6,32 @@ import 'package:flutter/material.dart';
 class AuthProvider with ChangeNotifier {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> test() async {
-    auth.authStateChanges().listen((User? user) {
-      if (user != null) {
-        log('user id from authStateChanges: ${user.uid}');
-      }
-    });
-
-    // final userCredential =
-    //     await FirebaseAuth.instance.signInWithCredential(credential);
-    // final user = userCredential.user;
-    // log('${user?.uid}');
-
-    if (auth.currentUser != null) {
-      log('current user from instance: ${auth.currentUser?.uid}');
-    }
-  }
-
   Future<void> signUpWithFirebaseUsingEmail(
       String emailAddress, String password) async {
     try {
-      final credential = await auth.createUserWithEmailAndPassword(
-        email: emailAddress,
-        password: password,
-      );
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailAddress, password: password);
 
-      log('$credential');
+      log('$userCredential');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        log('The password provided is too weak.');
+        print('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-        log('The account already exists for that email.');
+        print('The account already exists for that email.');
       }
     } catch (e) {
-      log('$e');
+      print(e);
     }
   }
 
   Future<void> signInWithFirebaseUsingEmail(
       String emailAddress, String password) async {
     try {
-      final credential = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: emailAddress, password: password);
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
       log('$credential');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
