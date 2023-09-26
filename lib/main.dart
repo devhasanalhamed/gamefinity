3a534ca7c85817924314aa832a945c2f5a97559e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-26 14:56:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -17,7 +16,6 @@ void main() async {
   await Firebase.initializeApp();
   await FirebaseAPI().initNotifications();
   await SharedPref.initSharedPref();
-
   runApp(const MyApp());
 }
 
