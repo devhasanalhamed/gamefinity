a21bd35f7a51f11c0b2df25e3d97a280a2dee616
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-14 20:34:45 +0300

    Create game_view_model.dart

---
+++ lib/features/game/data/game_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import 'model/game_model.dart';
+
+class GameViewModel extends ChangeNotifier {
+  List<GameModel> gameList = [];
+
+  Future<void> getGameList() async {
+    print("heel");
+    final dio = Dio();
+
+    final result = await dio.get('https://api.rawg.io/api/games?key=');
+
+    final responseData = result.data['results'] as List;
+    gameList.addAll(
+      responseData.map(
+        (game) => GameModel.fromJson(game),
+      ),
+    );
+
+    notifyListeners();
+  }
+}
