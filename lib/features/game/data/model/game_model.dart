47e6445bf5370944ac9861a8a0c6e6b956dc0568
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-15 19:12:55 +0300

    Update game_model.dart

--- lib/features/game/data/model/game_model.dart
+++ lib/features/game/data/model/game_model.dart
@@ -1,9 +1,15 @@
+import 'package:gamefinity/features/game/data/model/tag_model.dart';
+
+import 'genre_model.dart';
+
 class GameModel {
   final int id;
   final String name;
   final String backgroundImage;
   final String released;
   final double rating;
+  final List<Tag> tags;
+  final List<Genre> genres;
 
   GameModel({
     required this.id,
@@ -11,6 +17,8 @@ class GameModel {
     required this.backgroundImage,
     required this.released,
     required this.rating,
+    required this.tags,
+    required this.genres,
   });
 
   factory GameModel.fromJson(Map<String, dynamic> json) {
@@ -20,6 +28,16 @@ class GameModel {
       backgroundImage: json['background_image'],
       released: json['released'],
       rating: json['rating'],
+      genres: List.from(
+        (json['genres'] as List).map(
+          (genre) => Genre.fromJson(genre),
+        ),
+      ),
+      tags: List.from(
+        (json['tags'] as List).map(
+          (tag) => Tag.fromJson(tag),
+        ),
+      ),
     );
   }
 
