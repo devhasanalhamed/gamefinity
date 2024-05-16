7efcb30d8405131f1a3b0cf5736e2c394ff6a36d
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-16 12:58:02 +0300

    test time consuming
    testing purposes

--- lib/features/game/data/datasource/game_data_source.dart
+++ lib/features/game/data/datasource/game_data_source.dart
@@ -10,16 +10,23 @@ class GameDataSource extends GameBaseRepository {
   @override
   Future<(Set<GameModel>, String?)> fetchGames(
       Map<String, dynamic> data) async {
+    print("looking for data");
+    final start1 = DateTime.now();
+
     try {
       final Response result =
           await client.getData('${ApiConstants.games}?page=${data['page']}');
       //Todo: redo the comment
       // .getData('${ApiConstants.games}?page=${data['page'] ?? 1}');
       if (result.statusCode != 200) throw result;
+      print('data received: ${DateTime.now().difference(start1)}');
 
       final responseData = result.data['results'] as List;
+      final start = DateTime.now();
+      print('there is data');
       final gamesList =
           responseData.map((game) => GameModel.fromJson(game)).toSet();
+      print('data converted in: ${DateTime.now().difference(start)}');
       return (gamesList, null);
     } catch (e) {
       return (<GameModel>{}, e.toString());
